dd92acc172c731d9494a05ec995cb7b728e55729
jhomrocky <jhomrocky@gmail.com>
2017-12-11 18:11:07 -0500

    forgot to commit end results

---
+++ PetShop/Controllers/PetsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PetShop.Models;
+
+namespace PetShop.Controllers
+{
+    public class PetsController : Controller
+    {
+        private PetShopEntities db = new PetShopEntities();
+
+        // GET: Pets
+        public ActionResult Index()
+        {
+            return View(db.Pets.ToList());
+        }
+
+        // GET: Pets/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pet originalPet = db.Pets.Find(id);
+            if (originalPet == null)
+            {
+                return HttpNotFound();
+            }
+
+            var viewModel = new PetFoodViewModel //instance of Pet model (in this case needs 'pet' and 'food')
+            {
+                pet = originalPet,   //originalPet is from DB
+                food = (from food in db.Foods //"kinda" like foreach loop, 'food' is temporarily LINQ variable
+                       where food.FoodType.Contains(originalPet.PetType) // grabs type of FoodName containing PetType ('dog' in 'dog food')
+
+                       select food).ToList()
+            };
+            return View(viewModel); //returns entire viewModel (code right above this)
+        }
+
+        // GET: Pets/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PetID,PetName,PetType,PetWeight")] Pet pet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pets.Add(pet);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pet);
+        }
+
+        // GET: Pets/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pet pet = db.Pets.Find(id);
+            if (pet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pet);
+        }
+
+        // POST: Pets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PetID,PetName,PetType,PetWeight")] Pet pet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pet).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pet);
+        }
+
+        // GET: Pets/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pet pet = db.Pets.Find(id);
+            if (pet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pet);
+        }
+
+        // POST: Pets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pet pet = db.Pets.Find(id);
+            db.Pets.Remove(pet);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PetShop/Models/PetFoodViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetShop.Models
+{
+    public class PetFoodViewModel //viewmodel (this) is like a "container" of information being sent that is connected
+    {
+        public Pet pet { get; set; }
+        public ICollection<Food> food { get; set; }
+
+    }
+}
